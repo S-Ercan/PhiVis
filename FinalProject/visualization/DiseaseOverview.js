37095c8fb386d175e388ad4508548e222da5dcfb
Selman Ercan <ercan.s.selman@gmail.com>
2016-01-16 13:20:10 +0100

    Changed two visualization divs to just one. Linked to MagicSuggest CSS.

--- FinalProject/visualization/DiseaseOverview.js
+++ FinalProject/visualization/DiseaseOverview.js
@@ -1,6 +1,7 @@
 $(function()
 {
 	var ms1 = $('#overview').magicSuggest({
-	  data: ['New York','Los Angeles','Chicago','Houston','Philadelphia','Phoenix','San Antonio','San Diego','Dallas','San Jose','Jacksonville']
+	  data: ['New York','Los Angeles','Chicago','Houston','Philadelphia','Phoenix','San Antonio','San Diego','Dallas','San Jose','Jacksonville'],
+	  placeholder: 'Diseases...'
 	});
 });
