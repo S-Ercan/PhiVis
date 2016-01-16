ee6ad118f25fb63a1b492103424817b1122143e9
Saminobi <jpsamiotis@gmail.com>
2016-01-16 14:20:48 +0100

    PhiViz.js loads the data from .json file. Code structure changes in .html and .css.

--- FinalProject/visualization/PhiViz.js
+++ FinalProject/visualization/PhiViz.js
@@ -1,5 +1,24 @@
+function loadJSON(callback)
+{
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'phi.json', true);
+	xobj.onreadystatechange = function ()
+	{
+		if (xobj.readyState == 4 && xobj.status == "200")
+			callback(xobj.responseText);
+	};
+	xobj.send(null);
+}
+
+loadJSON(function(response)
+{
+	var diseases = JSON.parse(response);
+	console.log(diseases);
+});
+
 function forceGraph() {
-  
+
 
 }
 
