0ac79c828917baee597f71559643007c80940fce
Saminobi <jpsamiotis@gmail.com>
2016-01-16 15:57:58 +0100

    PhiViz.js, now saves the diseases in an object which groups duplicates by diseases.

--- FinalProject/visualization/PhiViz.js
+++ FinalProject/visualization/PhiViz.js
@@ -14,20 +14,29 @@ function loadJSON(callback)
 loadJSON(function(response)
 {
 	var phi = JSON.parse(response);
-	var selectedDiseases = [];
+	var diseases = {};
 
+	var disease;
 	for (var i = 0; i < phi.length; i++) {
-		selectedDiseases.push({id: phi[i].Identifier, name: phi[i].Disease});
-	}
-	console.log(selectedDiseases);
+		disease = phi[i].Disease;
+		if (disease in diseases) {
+			diseases[disease].push(phi[i]);
+		}
+		else {
+			diseases[disease] = [phi[i]];
+		}
 
+	}
+	console.log(diseases);
+
+	//var diseaseNames = diseases.keys();
 	$(function()
 	{
 		var ms1 = $('#overview').magicSuggest({
-		  data: selectedDiseases,
+		  data: Object.keys(diseases),
 		  placeholder: 'Diseases...'
 		});
-		
+
 		$(ms1).on('selectionchange', function(e, m) {
 			ms1.setSelection([]);
 		});
