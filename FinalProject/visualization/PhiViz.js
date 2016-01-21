eeb9502f0c0405c65ff3f334b12d6ae3ce51ee7a
Saminobi <jpsamiotis@gmail.com>
2016-01-21 15:24:56 +0100

    Force layout of 2 nodes, appears when the appropriate button is clicked. The nodes have images and labels and can be moved by the user. Change of bg colour for clarity.

--- FinalProject/visualization/PhiViz.js
+++ FinalProject/visualization/PhiViz.js
@@ -1,5 +1,5 @@
 $("#stackBarsButton").click(function() {
-	var rows = $('#selectedOverview').find('.jtable-data-row')
+	var rows = $('#selectedOverview').find('.jtable-data-row');
 	var selectedDiseases = {};
 	var diseaseName;
 	$.each(rows, function() {
@@ -30,6 +30,7 @@ loadJSON(function(response)
 	for (var i = 0; i < phi.length; i++) {
 		disease = phi[i].Disease;
 		gene = phi[i].Gene;
+	 	phenotype= phi[i].MutantPhenotype;
 		if (disease in diseases) {
 			diseases[disease].push(phi[i]);
 		}
@@ -50,7 +51,7 @@ loadJSON(function(response)
 			if(records.length > 0)
 			{
 				addDisease(records[0]);
-				ms.setSelection([]);	
+				ms.setSelection([]);
 			}
 		});
 	});
@@ -66,13 +67,14 @@ function addDisease(disease)
 	});
 }
 
+// ~~~~~~ Stack Bars ~~~~~~~
 function stackBars(diseases) {
 	var genes = [];
 	for (var key in diseases) {
 		var disease = diseases[key];
 		for (var i in disease)
 		{
-			genes.push(disease[i].Gene);	
+			genes.push(disease[i].Gene);
 		}
 	}
 	genes = genes.sort().filter(function(item, index, array) {
@@ -91,7 +93,7 @@ function stackBars(diseases) {
     height = 500 - margin.top - margin.bottom;
 
 	var x = d3.scale.ordinal()
-		.rangeRoundBands([0, width], .1);
+		.rangeRoundBands([0, width], 0.1);
 
 	var y = d3.scale.linear()
 		.rangeRound([height, 0]);
@@ -179,6 +181,64 @@ function stackBars(diseases) {
 		.text(function(d) { return d; });
 }
 
+// ~~~~~~ Force Graph ~~~~~~~
 function forceGraph() {
+	var width = 960,
+	    height = 500;
 
+	var nodes = [
+    { x:   width/3, y: height/2, name: "Hi" },
+    { x: 2*width/3, y: height/2, name: "Hello" }
+	];
+
+	var links = [
+    { source: 0, target: 1 }
+	];
+
+	var svg = d3.select("#graphViz").append("svg")
+	    .attr("width", width)
+	    .attr("height", height);
+
+	var force = d3.layout.force()
+			.gravity(0.05)
+			.distance(100)
+			.charge(-100)
+			.size([width, height]);
+
+	force
+			.nodes(nodes)
+			.links(links)
+			.start();
+
+	var link = svg.selectAll(".link")
+	      .data(links)
+	    	.enter().append("line")
+	      .attr("class", "link");
+
+	var node = svg.selectAll(".node")
+	      .data(nodes)
+	    	.enter().append("g")
+	      .attr("class", "node")
+	      .call(force.drag);
+
+	node.append("image")
+      .attr("xlink:href", "https://github.com/favicon.ico")
+      .attr("x", -8)
+      .attr("y", -8)
+      .attr("width", 16)
+      .attr("height", 16);
+
+	node.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name; });
+
+	force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			});
 }
